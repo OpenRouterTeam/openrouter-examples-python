a495e724ceab667e76833c009150813e3de447ac
L❤️ ☮️ ✋ <6723574+louisgv@users.noreply.github.com>
2023-11-13 22:01:56 -0400

    Add python example with poetry

---
+++ src/openai_test.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env -S poetry run python
+
+from openai import OpenAI
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+# gets API Key from environment variable OPENAI_API_KEY
+client = OpenAI(
+    base_url=getenv("OPENROUTER_BASE_URL"),
+    api_key=getenv("OPENROUTER_API_KEY"),
+)
+
+# Non-streaming:
+print("----- standard request -----")
+completion = client.chat.completions.create(
+    extra_headers={"HTTP-Referer": "https://openrouter.ai"},
+    model="gpt-4",
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        },
+    ],
+)
+print(completion.choices[0].message.content)
+
+# Streaming:
+print("----- streaming request -----")
+stream = client.chat.completions.create(
+    extra_headers={"HTTP-Referer": "https://openrouter.ai"},
+    model="gpt-4",
+    messages=[
+        {
+            "role": "user",
+            "content": "How do I output all files in a directory using Python?",
+        },
+    ],
+    stream=True,
+)
+for chunk in stream:
+    if not chunk.choices:
+        continue
+
+    print(chunk.choices[0].delta.content, end="")
+print()
